83c55d7de8394108039555d304159db13c49b69b
Justin Duong <jduong@umich.edu>
2022-07-06 13:42:12 -0400

    Added active states for buttons.

--- js/comments.js
+++ js/comments.js
@@ -1,3 +1,18 @@
+function createSVG(width, height, className, d, fill) {
+  const svgEle = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svgEle.setAttributeNS(null, 'width', width);
+  svgEle.setAttributeNS(null, 'height', height);
+  svgEle.classList.add(className);
+
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttributeNS(null, 'd', d);
+  path.setAttributeNS(null, 'fill', fill);
+  
+  svgEle.append(path);
+  
+  return svgEle;
+}
+
 function createReplies(comment, currentUser) {
   const replies = comment.replies;
   const repliesListContainer = document.createElement('ul');
@@ -41,28 +56,28 @@ function createReplies(comment, currentUser) {
 
     const likesContainer = document.createElement('div');
     likesContainer.classList.add('likes-container')
-    const plusIcon = document.createElement('img');
-    plusIcon.src = '../images/icon-plus.svg';
-    plusIcon.classList.add('icon-plus');
-
-    const minusIcon = document.createElement('img');
-    minusIcon.src = '../images/icon-minus.svg';
-    minusIcon.classList.add('icon-minus');
+    
+    const plusIcon = createSVG('11', '11', 'icon-plus', 'M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z', '#C5C6EF');
+    const minusIcon = createSVG('11', '3', 'icon-minus', 'M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z', '#C5C6EF');
 
     const likes = document.createElement('div');
     likes.innerText = reply.score;
 
     likesContainer.append(plusIcon, likes, minusIcon);
 
+    const editCommentsContainer = document.createElement('div');
+    editCommentsContainer.classList.add('edit-comments-container');
+
     const replyContainer = document.createElement('div');
     replyContainer.classList.add('reply-container');
-
     const replyIcon = document.createElement('img');
     replyIcon.src = '../images/icon-reply.svg';
     replyIcon.classList.add('icon-reply');
 
     replyContainer.innerText = 'Reply';
     replyContainer.prepend(replyIcon);
+
+    editCommentsContainer.append(replyContainer);
 
     if (reply.user.username === currentUser.username) {
       const deleteDiv = document.createElement('div');
@@ -74,10 +89,10 @@ function createReplies(comment, currentUser) {
       deleteImg.classList.add('icon-delete');
       deleteDiv.prepend(deleteImg);
 
-      replyContainer.prepend(deleteDiv);
+      editCommentsContainer.prepend(deleteDiv);
     }
 
-    commentContainer.append(commentUserDiv, commentContent, likesContainer, replyContainer);
+    commentContainer.append(commentUserDiv, commentContent, likesContainer, editCommentsContainer);
     itemElement.append(commentContainer);
 
     repliesListContainer.append(itemElement);
@@ -123,28 +138,28 @@ function createComments(obj) {
 
     const likesContainer = document.createElement('div');
     likesContainer.classList.add('likes-container')
-    const plusIcon = document.createElement('img');
-    plusIcon.src = '../images/icon-plus.svg';
-    plusIcon.classList.add('icon-plus');
 
-    const minusIcon = document.createElement('img');
-    minusIcon.src = '../images/icon-minus.svg';
-    minusIcon.classList.add('icon-minus');
+    const plusIcon = createSVG('11', '11', 'icon-plus', 'M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z', '#C5C6EF');
+    const minusIcon = createSVG('11', '3', 'icon-minus', 'M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z', '#C5C6EF');
 
     const likes = document.createElement('div');
     likes.innerText = comment.score;
 
     likesContainer.append(plusIcon, likes, minusIcon);
 
+    const editCommentsContainer = document.createElement('div');
+    editCommentsContainer.classList.add('edit-comments-container');
+
     const replyContainer = document.createElement('div');
     replyContainer.classList.add('reply-container');
-
     const replyIcon = document.createElement('img');
     replyIcon.src = '../images/icon-reply.svg';
     replyIcon.classList.add('icon-reply');
 
     replyContainer.innerText = 'Reply';
     replyContainer.prepend(replyIcon);
+
+    editCommentsContainer.append(replyContainer);
 
     if (comment.user.username === obj.currentUser.username) {
       const deleteDiv = document.createElement('div');
@@ -156,10 +171,10 @@ function createComments(obj) {
       deleteImg.classList.add('icon-delete');
       deleteDiv.prepend(deleteImg);
 
-      replyContainer.prepend(deleteDiv);
+      editCommentsContainer.prepend(deleteDiv);
     }
 
-    commentContainer.append(commentUserDiv, commentContent, likesContainer, replyContainer);
+    commentContainer.append(commentUserDiv, commentContent, likesContainer, editCommentsContainer);
     itemElement.append(commentContainer);
 
     if (comment.replies.length !== 0) {
